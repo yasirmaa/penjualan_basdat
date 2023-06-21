58cc9b3dd17c05f91644a2bffbb4001dbaf46267
yasirmaa <yasirmaarif26@gmail.com>
2023-06-21 22:50:21 +0700

    ubah title index

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $pesanan = pg_fetch_assoc($pesanan);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yamapedia</title>
+    <title>Yamapedia Dashboard</title>
     <!-- CSS -->
     <link rel="stylesheet" href="./css/style.css?v=<?php echo time(); ?>">
     <!-- Bootsrap5 -->
